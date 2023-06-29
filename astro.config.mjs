ad3c6cbb95b4ffe00c70023023b69d4030cf3a63
Anawach Anantachoke <achinhchin@Thakoons-MacBook-Air.local>
2023-06-29 22:57:29 +0700

    ana: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true
+  })
 });
